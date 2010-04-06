32b38e42f5629ff0c99c1188006b1a4327027293
amerzky <amerzky@3518cec0-bdc5-4d90-bc36-c327ef005074>
2010-04-06 18:53:05 +0000

    file -> filesystem
    git-svn-id: file://localhost/N/u/merzky/svn2git/data/saga-cpp-binding-python/svn/bindings/python/trunk@4807 3518cec0-bdc5-4d90-bc36-c327ef005074

--- test/python/suite/packages/__init__.py
+++ test/python/suite/packages/__init__.py
@@ -4,6 +4,6 @@
 # License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
-from file.file import * 
-from job.job   import *
+from filesystem.file import * 
+from job.job         import *
 
