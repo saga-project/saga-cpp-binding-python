9906cba9f12477df63a5031f016c33c419689ce6
oweidner <oweidner@3518cec0-bdc5-4d90-bc36-c327ef005074>
2011-06-01 22:15:57 +0000

    job example that doesn't work. see ticket: http://faust.cct.lsu.edu/trac/saga/ticket/750
    git-svn-id: file://localhost/N/u/merzky/svn2git/data/saga-cpp-binding-python/svn/bindings/python/trunk@5263 3518cec0-bdc5-4d90-bc36-c327ef005074

--- examples/job/simple_example.py
+++ examples/job/simple_example.py
@@ -1,4 +1,4 @@
-## Copyright (c) 2010 Ole Weidner, Louisiana State University
+## Copyright (c) 2011 Ole Weidner, Louisiana State University
 ##
 ## This is an example of how to submit an MPI job through Globus GRAM. 
 ## A detailed description can be found at 
@@ -7,17 +7,26 @@
 import saga
 
 try:
+  # create an "echo 'hello, world' job"
   jd = saga.job.description()
+  jd.executable = "/bin/echo"
+  jd.arguments  = ["Hello, World!"]
+  jd.interactive  = True
+
   
-  jd.executable = "/bin/hostname"
-  jd.output     = "stdout.log"
-  
+  # connect to the local job service
   js = saga.job.service("fork://localhost");
 
+  # submit the job
   job = js.create_job(jd)
   job.run()
+
+  # wait for the job to complete
   job.wait(-1)
   
-except saga.exception, e:
-  print e.get_all_messages()
+  # print the job's output
+  print job.get_stdout()
   
+except saga.exception, e:
+  print "ERROR: " 
+  print e.get_all_messages()
