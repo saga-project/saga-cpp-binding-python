7643f3ed30a097015501ac6a273b6443fa16b66c
oweidner <oweidner@3518cec0-bdc5-4d90-bc36-c327ef005074>
2011-07-14 15:58:15 +0000

    updated documentation
    git-svn-id: file://localhost/N/u/merzky/svn2git/data/saga-cpp-binding-python/svn/bindings/python/trunk@5320 3518cec0-bdc5-4d90-bc36-c327ef005074

--- packages/advert/__init__.py
+++ packages/advert/__init__.py
@@ -4,16 +4,25 @@
 #  Distributed under the Boost Software License, Version 1.0. (See accompanying
 #  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-# This file must be located inside a directory named SAGA/advert which in turn 
+# XXX This file must be located inside a directory named SAGA/advert which in turn 
 # should be located in a directory listed in your PYTHON_PATH
 
 from _advert import *         # import advert package
 
 class directory(_advert.directory):
+"""
+An advert directory. It can contain other advert directories or entries.
+"""
   pass
   
 class entry(_advert.entry):
   pass
+"""
+An advert entry.
+"""
   
 class flags(_advert.flags):
+"""
+Flags that can be used with the saga advert package. 
+"""
   pass
